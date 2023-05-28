d59276f6a826f20e7797c2e4a2623c70ea962a52
JasonEmanuel <jemanuel@student.ciputra.ac.id>
2023-05-28 16:52:00 +0700

    binding todolistActivity

--- app/src/main/java/dev/kchr/se/todolistActivity.kt
+++ app/src/main/java/dev/kchr/se/todolistActivity.kt
@@ -2,10 +2,15 @@ package dev.kchr.se
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.kchr.se.databinding.ActivityMainBinding
+import dev.kchr.se.databinding.ActivityTodolistBinding
 
 class todolistActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTodolistBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todolist)
+        binding = ActivityTodolistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
