911a02d44d506898dfdcb7833c32377ec3709005
itrinia <isantoso@student.ciputra.ac.id>
2023-05-28 14:44:41 +0700

    main activity

--- app/src/main/java/dev/kchr/se/MainActivity.kt
+++ app/src/main/java/dev/kchr/se/MainActivity.kt
@@ -7,9 +7,14 @@ import dev.kchr.se.databinding.ActivitySplashScreenBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Menggunakan ActivityMainBinding untuk menghubungkan tampilan layout dengan activity
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Menetapkan tampilan root dari layout yang dihubungkan ke setContentView
         setContentView(binding.root)
     }
 }
